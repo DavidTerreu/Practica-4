86142ea8ca676eae039ed2a542a664ea4c4e4202
David <davidterreu@hotmail.com>
2026-01-19 10:16:42 +0100

    Añade columnas y sus nombres según se vayan poniendo

--- main.js
+++ main.js
@@ -4,22 +4,37 @@ const colum = document.getElementById('colum');
 const nom = document.getElementById('nom');
 const limpiar = document.getElementById('limpiar');
 const label = document.getElementById('labelN');
+const labelLimite = document.getElementById('labelL');
 const formu = document.getElementById('form');
+const limite = document.getElementById('limite');
+const botonL = document.getElementById('botonL');
+const tabla = document.getElementById('tabla');
 let maxC = 0;
 let contC = 1;
 
+// Cargar contador de columnas desde localStorage si existe
 if (localStorage.getItem('numColum')) {
     contC = JSON.parse(localStorage.getItem('numColum'));
 } else {
     localStorage.setItem('numColum', JSON.stringify(contC));
 }
 
+// Cargar número máximo de columnas desde localStorage si existe
 if (localStorage.getItem('Numero Columnas:')) {
     maxC = parseInt(JSON.parse(localStorage.getItem('Numero Columnas:')));
 }
 
+// Actualizar labels con el contador actual
 label.textContent = `Nombre columna ${contC}:`;
+labelLimite.textContent = `Límite columna ${contC}:`;
 
+// Deshabilitar input límite al principio para que la primera siempre sea sin limite
+if (contC <2){
+    limite.disabled = true;
+    botonL.disabled = true;
+}
+
+// Cargar valores temporales desde localStorage si existen
 if (localStorage.getItem('temporalColumna')) {
     colum.value = localStorage.getItem('temporalColumna');
 }
@@ -28,16 +43,19 @@ if (localStorage.getItem('temporalNombre')) {
     nom.value = localStorage.getItem('temporalNombre');
 }
 
+// Verificar si el input columna debe esta ocultos o deshabilitados
 if (localStorage.getItem('inputOculto') === 'true') {
     colum.disabled = true;
     botonC.disabled = true;
     colum.value = '';
 }
 
+// Verificar si el formulario debe estar oculto
 if (localStorage.getItem('columnaOculta') === 'true') {
     formu.style.display = 'none';
 }
 
+//Funciones INPUTS
 function numCol(e) {
     if (colum.value.trim() !== '') {
         localStorage.setItem('temporalColumna', colum.value);
@@ -58,6 +76,17 @@ function nomCol(e) {
 
 nom.addEventListener('input', nomCol);
 
+function limCol(e) {
+    if (limite.value.trim() !== '') {
+        localStorage.setItem('temporalLimite', limite.value);
+    } else {
+        localStorage.removeItem('temporalLimite');
+    }
+}
+
+limite.addEventListener('input', limCol);
+
+//Funciones BOTONES
 function añadirColumna(e) {
     e.preventDefault();
     alert(`Has añadido ${colum.value} columnas.`);
@@ -66,14 +95,18 @@ function añadirColumna(e) {
     maxC = parseInt(JSON.parse(localStorage.getItem('Numero Columnas:')));
     alert(`Contador después: ${maxC}`);
 
+    // Crear divs según el número de columnas elegido
+    tabla.innerHTML = '';
+    for (let i = 1; i <= maxC; i++) {
+        const divColumna = document.createElement('div');
+        divColumna.id = `columna-${i}`;
+        tabla.appendChild(divColumna);
+    }
+
     colum.disabled = true;
     botonC.disabled = true;
 
     localStorage.setItem('inputOculto', 'true');
-
-    /*colum.style.display = 'none';
-    document.querySelector('label[for="columnas"]').style.display = 'none';
-    botonC.style.display = 'none';*/
 }
 
 botonC.addEventListener('click', añadirColumna);
@@ -90,6 +123,15 @@ function añadirNombre(e) {
 
     alert(`Has añadido ${nom.value} como nombre de columna.`);
     localStorage.setItem(`Nombre Columna ${contC}:`, JSON.stringify(nom.value));
+
+    //Añadir el nombre de la columna al div correspondiente
+    const H3Columna = document.createElement('h3');
+    H3Columna.textContent = nom.value;
+    const columnaActual = document.getElementById(`columna-${contC}`);
+    if (columnaActual) {
+        columnaActual.appendChild(H3Columna);
+    }
+
     contC++;
     alert(`Contador después: ${contC}`);
     nom.value = '';
@@ -98,12 +140,13 @@ function añadirNombre(e) {
     localStorage.setItem('numColum', JSON.stringify(contC));
 
     label.textContent = `Nombre columna ${contC}:`;
+    labelLimite.textContent = `Límite columna ${contC}:`;
+
+    limite.disabled = false;
+    botonL.disabled = false;
 
     if (contC > maxC) {
         formu.style.display = 'none';
-        /*nom.style.display = 'none';
-        document.querySelector('label[for="nombre"]').style.display = 'none';
-        botonN.style.display = 'none';*/
         localStorage.setItem('columnaOculta', 'true');
     }
 }
@@ -114,10 +157,13 @@ function limpiarDatos() {
     localStorage.clear();
     alert("Se han eliminado todos los datos guardados.");
     contC = 1;
+    maxC = 0;
 
     formu.style.display = '';
+    tabla.innerHTML = '';
 
     label.textContent = `Nombre columna ${contC}:`;
+    labelLimite.textContent = `Límite columna ${contC}:`;
 
     localStorage.setItem('inputOculto', 'false');
     colum.disabled = false;
@@ -125,6 +171,10 @@ function limpiarDatos() {
     localStorage.setItem('columnaOculta', 'false');
     colum.value = '';
     nom.value = '';
+    limite.value = '';
+
+    limite.disabled = true;
+    botonL.disabled = true;
 }
 
 limpiar.addEventListener('click', limpiarDatos);
